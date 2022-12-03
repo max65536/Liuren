17fa9bdbc4d2da0102a47b454b67e2daf879486c
max65536 <maxinteger65536@gmail.com>
2022-12-03 07:07:30 +0100

    init printer, 课体，毕法赋

---
+++ analyzer.py
@@ -0,0 +1,12 @@
+
+def check_keti():
+    '''
+    课体
+    '''
+    pass
+
+def check_bifa():
+    '''
+    毕法赋
+    '''
+    pass
---
+++ printer.py
@@ -0,0 +1,12 @@
+
+from LiuRenPan import LiuRenPan
+
+class Printer(object):
+    def __init__(self, liurenpan, format) -> None:
+        pass
+
+    def to_string(self):
+        pass
+
+    def to_fig(self):
+        pass
